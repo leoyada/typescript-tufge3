dbf1713a5b472b9f76886f2f00dcd9ce4f05a04c
leoyada <eaalekar@students.eaaa.dk>
2021-12-23 14:02:28 +0100

    Make this thing stackblitz friendly

---
+++ index.ts
@@ -0,0 +1,14 @@
+// Import stylesheets
+import './style.css';
+
+const button = document.querySelector('.enter');
+const overlay = document.querySelector('.overlay');
+
+button.addEventListener('click', () => {
+  overlay.classList.add('hide');
+  overlay.addEventListener('transitionend', (event) => {
+    if (event.target === overlay) {
+      document.body.classList.add('scroll');
+    }
+  });
+});
